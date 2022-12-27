a4a256c4f5ad3fa168a8c70236551ab46ac174f0
MirkoR89 <mirko.rinelli@gmail.com>
2022-12-27 14:10:11 +0100

    chore: sum of base experience

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -6,13 +6,20 @@ const Deck = ({ openDeck, setOpenDeck }) => {
 
     const { deck } = useSelector(state => state.pokemon)
 
-    console.log(deck);
+    let baseExperience = 0
+    deck.forEach(pokemon => {
+        baseExperience += pokemon.base_experience
+    })
 
 
     return (
         <div className="flex flex-col justify-center items-center gap-y-10">
             <h1 className="text-6xl font-bold">My Deck</h1>
             <ButtonDeck pokemon={true} openDeck={openDeck} setOpenDeck={setOpenDeck} />
+            <div className="flex gap-x-1">
+                <span>Base Experice</span>
+                <span>{baseExperience}</span>
+            </div>
             <div className="flex justify-center flex-wrap gap-10 p-3">
                 {
                     deck?.map(p =>
